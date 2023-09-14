70cbb4bcaccc92e84f87c818f68d5154128391e3
nkc20236015 <nkc23.ikeda.sora@gamil.com>
2023-09-14 15:17:35 +0900

    9/14

---
+++ Assets/TitleItem/StartButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
